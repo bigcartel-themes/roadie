0276065c593891f0c43ae55edaebbf235d5f7071
Nick Endle <nick.endle@gmail.com>
2020-02-28 16:17:14 -0600

    Implement image magnification and zoom features

--- source/javascripts/product.js
+++ source/javascripts/product.js
@@ -80,6 +80,107 @@ function disableSelectOption(select_option, type) {
 }
 
 
+var driftOptions = {
+  // Prefix for generated element class names (e.g. `my-ns` will
+  // result in classes such as `my-ns-pane`. Default `drift-`
+  // prefixed classes will always be added as well.
+  namespace: null,
+  // Whether the ZoomPane should show whitespace when near the edges.
+  showWhitespaceAtEdges: false,
+  // Whether the inline ZoomPane should stay inside
+  // the bounds of its image.
+  containInline: false,
+  // How much to offset the ZoomPane from the
+  // interaction point when inline.
+  inlineOffsetX: 0,
+  inlineOffsetY: 0,
+  // A DOM element to append the inline ZoomPane to.
+  inlineContainer: document.body,
+  // Which trigger attribute to pull the ZoomPane image source from.
+  sourceAttribute: 'data-zoom',
+  // How much to magnify the trigger by in the ZoomPane.
+  // (e.g., `zoomFactor: 3` will result in a 900 px wide ZoomPane image
+  // if the trigger is displayed at 300 px wide)
+  zoomFactor: 3,
+  // A DOM element to append the non-inline ZoomPane to.
+  // Required if `inlinePane !== true`.
+  paneContainer: document.body,
+  // When to switch to an inline ZoomPane. This can be a boolean or
+  // an integer. If `true`, the ZoomPane will always be inline,
+  // if `false`, it will switch to inline when `windowWidth <= inlinePane`
+  inlinePane: true,
+  // If `true`, touch events will trigger the zoom, like mouse events.
+  handleTouch: false,
+  // If present (and a function), this will be called
+  // whenever the ZoomPane is shown.
+  onShow: null,
+  // If present (and a function), this will be called
+  // whenever the ZoomPane is hidden.
+  onHide: null,
+  // Add base styles to the page. See the "Theming"
+  // section of README.md for more information.
+  injectBaseStyles: true,
+  // An optional number that determines how long to wait before
+  // showing the ZoomPane because of a `mouseenter` event.
+  hoverDelay: 0,
+  // An optional number that determines how long to wait before
+  // showing the ZoomPane because of a `touchstart` event.
+  // Setting this to a reasonable amount will allow users to execute
+  // scroll-gestures events on touch-enabled devices with the image as
+  // a starting point
+  touchDelay: 0,
+  // If true, a bounding box will show the area currently being previewed
+  // during mouse hover
+  hoverBoundingBox: false,
+  // If true, a bounding box will show the area currently being previewed
+  // during touch events
+  touchBoundingBox: false,
+  // A DOM element to append the bounding box to.
+  boundingBoxContainer: document.body,
+};
+
+new Drift(document.querySelector('.magnify-on-hover .primary-product-image'), driftOptions);
+
+var luminousOptions = {
+  // Prefix for generated element class names (e.g. `my-ns` will
+  // result in classes such as `my-ns-lightbox`. Default `lum-`
+  // prefixed classes will always be added as well.
+  namespace: null,
+  // Which attribute to pull the lightbox image source from.
+  sourceAttribute: "href",
+  // Captions can be a literal string, or a function that receives the Luminous instance's trigger element as an argument and returns a string. Supports HTML, so use caution when dealing with user input.
+  caption: null,
+  // The event to listen to on the _trigger_ element: triggers opening.
+  openTrigger: "click",
+  // The event to listen to on the _lightbox_ element: triggers closing.
+  closeTrigger: "click",
+  // Allow closing by pressing escape.
+  closeWithEscape: true,
+  // Automatically close when the page is scrolled.
+  closeOnScroll: false,
+  // Disable close button
+  showCloseButton: false,
+  // A node to append the lightbox element to.
+  appendToNode: document.body,
+  // A selector defining what to append the lightbox element to.
+  // This will take precedence over `appendToNode`.
+  appendToSelector: null,
+  // If present (and a function), this will be called
+  // whenever the lightbox is opened.
+  onOpen: null,
+  // If present (and a function), this will be called
+  // whenever the lightbox is closed.
+  onClose: null,
+  // When true, adds the `imgix-fluid` class to the `img`
+  // inside the lightbox. See https://github.com/imgix/imgix.js
+  // for more information.
+  includeImgixJSClass: false,
+  // Add base styles to the page. See the "Theming"
+  // section of README.md for more information.
+  injectBaseStyles: true
+};
+
+new Luminous(document.querySelector(".click-to-zoom .product-image-zoom"), luminousOptions);
 
 
 $('.product-form').submit(function(e) {
@@ -131,9 +232,9 @@ $('.product-form').submit(function(e) {
 
 $(document).ready(function() {
   if ($('.all-similar-products').length) {
-    var num_products = $('.all-similar-products > a').length;
+    var num_products = $('.all-similar-products .product-list-item').length;
     var elements = $('.all-similar-products').children().toArray();
-    var num_to_display = 3;
+    var num_to_display = $('.related-products-container').data('num-products');
     for (var i=1; i<=num_to_display; i++) {
       var randomIndex = getRandomIndex(elements);
       $('.similar-product-list').append($('.all-similar-products').children().eq(randomIndex));
