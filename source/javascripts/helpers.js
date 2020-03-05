8fc25a865165e2b426f7d14f62d7e3731732de56
Nick Endle <nick.endle@gmail.com>
2020-03-05 09:58:39 -0600

    Move announce message JS into layout file to speed things up

--- source/javascripts/helpers.js
+++ source/javascripts/helpers.js
@@ -1,3 +1,5 @@
+
+
 var isGreaterThanZero = function(currentValue) {
   return currentValue > 0;
 }
@@ -89,41 +91,7 @@ function autoExpand(textarea) {
     }
   }
 };
-String.prototype.hashCode = function() {
-  var hash = 0, i, chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr   = this.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0;
-  }
-  return hash;
-};
 
-function setCookie(name,value,days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-function eraseCookie(name) {
-  document.cookie = name+'=; Max-Age=-99999999;';
-}
 function getRandomIndex(elements) {
   return Math.floor(Math.random() * elements.length);
 }
