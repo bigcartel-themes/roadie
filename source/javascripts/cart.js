df118b24c9e0aaf7c52ac95329ef4d4ed2e27997
Nick Endle <nick.endle@gmail.com>
2020-05-12 12:47:03 -0500

    Fix cart behavior when changing quantity / pressing Enter on quantity input

--- source/javascripts/cart.js
+++ source/javascripts/cart.js
@@ -36,13 +36,25 @@ $('body')
       processUpdate('', item_id, '', cart);
     });
   })
-  .on( 'blur','.option-quantity', function(e) {
+  .on('change','.option-quantity', function(e) {
     var item_id = $(this).parent().data("item-id");
     var new_val = $(this).val();
     var input = $(this);
     Cart.updateItem(item_id, new_val, function(cart) {
       processUpdate(input, item_id, new_val, cart);
     });
+  })
+  .on('keydown','.option-quantity', function(e) {
+    if (e.keyCode == 13) {
+      item_id = $(this).closest('.cart-item').data("item-id");
+      new_val = $(this).val();
+      input = $(this);
+      Cart.updateItem(item_id, new_val, function(cart) {
+        processUpdate(input, item_id, new_val, cart);
+      });
+      e.preventDefault();
+      return false;
+    }
   })
 
 var updateCart = function(cart) {
