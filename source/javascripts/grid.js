ce5418b438143b68ed190f407fa27c846c99a8d4
Nick Endle <nick.endle@gmail.com>
2020-06-08 11:58:21 -0500

    Use lazysizes for lazy loading and responsive image loading
    Lazysizes will detect the best image to use depending on the screen size. This includes a few plugins to handle blurring the lower quality image placeholder, automatically handle aspect ratio for object-fit, and attribute changing (for when small thumbnails are clicked to replace the larger primary image). This change replaces the existing lazy load that used intersection observer.

--- source/javascripts/grid.js
+++ source/javascripts/grid.js
@@ -26,26 +26,3 @@ $(window).on("load resize", function() {
     }
   }
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-  if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          let lazyImage = entry.target;
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.srcset = lazyImage.dataset.srcset;
-          lazyImage.classList.remove("lazy");
-          lazyImageObserver.unobserve(lazyImage);
-        }
-      }, {
-        rootMargin: "0px 0px 256px 0px"
-      });
-    });
-
-    lazyImages.forEach(function(lazyImage) {
-      lazyImageObserver.observe(lazyImage);
-    });
-  }
-});
